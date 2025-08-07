ecb975bd31d0284347ead3edde3d78e5cf2b390f
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-08-07 23:11:41 +0200

    move sharexconfig to routes/

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser');
-const crypto = require('crypto');
-const bcrypt = require('bcrypt');
 const path = require('path')
-const { supabase } = require('./db.js');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pages', 'views')); // assumes /pages/image.ejs
-
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.static(path.join(process.cwd(), 'src', 'public')));
 
 if (process.env.LOCKED === 'true') {
@@ -21,34 +20,17 @@ if (process.env.LOCKED === 'true') {
 	});
 }
 
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
-
-// ShareX Config download
-app.get('/sharexconfig', (req, res) => {
-  const protocol = req.protocol;
-  const host = req.get('host');
-  const config = {
-    "Version": "14.1.0",
-    "Name": "pissandshitimages",
-    "DestinationType": "ImageUploader",
-    "RequestMethod": "POST",
-    "RequestURL": `https://${req.get('host')}/upload`,
-    "Body": "MultipartFormData",
-    "FileFormName": "image",
-    "Arguments": {
-      "hide": "on"
-    },
-    "ResponseType": "RedirectionURL",
-    "URL": "{responseurl}"
-  };
-  
-  res.setHeader('Content-Disposition', 'attachment; filename="pissandshitimages.sxcu"');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(config, null, 2));
+app.use((req, res, next) => {
+	const forbiddenParams = ['env', 'process', 'secret'];
+	for (const param of forbiddenParams) {
+		if (req.query[param] !== undefined) {
+			return res.status(403).send('Forbidden');
+		}
+	}
+	next();
 });
+
 
 // static pages
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'pages', 'index.html')); });
@@ -62,22 +44,14 @@ const adminRouter = require('./routes/admin.js');
 const leaderboardRouter = require('./routes/leaderboard.js');
 const galleryRouter = require('./routes/gallery.js');
 const imageRouter = require('./routes/image.js');
+const sharexConfigRouter = require('./routes/sharexconfig.js');
 
 app.use('/', adminRouter);
 app.use('/', uploadRouter);
 app.use('/', leaderboardRouter);
 app.use('/', galleryRouter);
 app.use('/', imageRouter);
-
-app.use((req, res, next) => {
-	const forbiddenParams = ['env', 'process', 'secret'];
-	for (const param of forbiddenParams) {
-		if (req.query[param] !== undefined) {
-			return res.status(403).send('Forbidden');
-		}
-	}
-	next();
-});
+app.use('/', sharexConfigRouter);
 
 app.listen(PORT, () => {
   console.log(`pissandshitimages running on http://localhost:${PORT}`);
