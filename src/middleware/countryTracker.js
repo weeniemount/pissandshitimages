98cf5cd5ebeebcd086a93911caec4144b006b871
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-09-02 11:31:13 +0200

    Update countryTracker.js

--- src/middleware/countryTracker.js
+++ src/middleware/countryTracker.js
@@ -1,15 +1,26 @@
 const geoip = require('geoip-lite');
 
 function countryTracker(req, res, next) {
-    const ip = req.ip || req.connection.remoteAddress;
-    const geo = geoip.lookup(ip);
+    // Try to get the real IP address from various headers
+    const ip = (
+        req.headers['x-forwarded-for']?.split(',')[0] || 
+        req.headers['x-real-ip'] ||
+        req.ip ||
+        req.connection.remoteAddress
+    ).trim();
+
+    // Clean the IP address
+    const cleanIp = ip.replace(/^.*:/, '').replace(/[^0-9.]/g, '');
+    
+    // Look up the location
+    const geo = geoip.lookup(cleanIp);
     
     // Add country info to the request object
     req.countryInfo = {
         country: geo ? geo.country : 'Unknown',
         // We specifically don't include city/region/ll for privacy
     };
-    
+
     next();
 }
 
