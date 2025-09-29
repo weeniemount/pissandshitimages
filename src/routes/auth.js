4c3d12f2df014586ac73e11c0d113d943284e3cc
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-09-29 21:31:52 +0200

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,11 +5,23 @@ const { passport } = require('../middleware/discordAuth');
 authRouter.get('/auth/discord', passport.authenticate('discord'));
 
 authRouter.get('/auth/discord/callback', 
-    passport.authenticate('discord', {
-        failureRedirect: '/?error=auth_failed'
-    }), 
-    (req, res) => {
-        res.redirect('/');
+    (req, res, next) => {
+        passport.authenticate('discord', (err, user, info) => {
+            if (err) {
+                console.error('Discord auth error:', err);
+                return res.redirect('/?error=' + encodeURIComponent(err.message));
+            }
+            if (!user) {
+                return res.redirect('/?error=' + encodeURIComponent(info?.message || 'Authentication failed'));
+            }
+            req.logIn(user, (err) => {
+                if (err) {
+                    console.error('Login error:', err);
+                    return res.redirect('/?error=' + encodeURIComponent(err.message));
+                }
+                res.redirect('/');
+            });
+        })(req, res, next);
     }
 );
 
